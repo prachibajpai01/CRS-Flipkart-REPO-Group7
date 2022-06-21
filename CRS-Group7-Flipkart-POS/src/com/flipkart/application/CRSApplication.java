0c58b6ca7212c1ef797230ffd5c973099f607f46
Kanishka Gupta <kanishka.gupta@BLRETV-YC39MW.local>
2022-06-21 09:50:12 +0530

    CRS Application

--- CRS-Group7-Flipkart-POS/src/com/flipkart/application/CRSApplication.java
+++ CRS-Group7-Flipkart-POS/src/com/flipkart/application/CRSApplication.java
@@ -1,8 +1,123 @@
 package com.flipkart.application;
 
+import com.flipkart.constant.Role;
+import com.flipkart.service.StudentImpl;
+import com.flipkart.service.UserImpl;
+import java.util.Scanner;
+
 public class CRSApplication {
+    UserImpl userImpl=new UserImpl();
+    StudentImpl studentImpl=new StudentImpl();
+    static boolean loggedin = false;
 
-    public static void main(String args[]){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        CRSApplication crsApplication = new CRSApplication();
 
+        int userInput;
+        do
+        {
+            System.out.println("----------Welcome to CRS Application---------");
+            System.out.println("1. Login");
+            System.out.println("2. Student Registration");
+            System.out.println("3. Update password");
+            System.out.println("4. Exit");
+            System.out.println("Enter your choice :");
+            userInput=sc.nextInt();
+
+            switch(userInput)
+            {
+                case 1:
+                    crsApplication.loginUser();
+                    break;
+                case 2:
+                    crsApplication.registerStudent();
+                    break;
+                case 3:
+                    crsApplication.updatePassword();
+                    break;
+                default:
+                    System.out.println("Invalid Input. Please try again!!");
+            }
+        }while(userInput!=4);
+    }
+    public void loginUser() {
+        Scanner sc=new Scanner(System.in);
+        String userId,password;
+        System.out.println("Email:");
+        userId=sc.next();
+        System.out.println("Password:");
+        password=sc.next();
+
+        loggedin=userImpl.authenticate(userId,password);
+
+        if(loggedin) {
+            String role = userImpl.getRole(userId);
+            Role userRole = Role.stringToName(role);
+            switch (userRole) {
+                case ADMIN:
+                    AdminCRSMenu adminCRSMenu=new AdminCRSMenu();
+                    adminCRSMenu.createMenu();
+                case PROFESSOR:
+                    //call menu
+                case STUDENT:
+                    int studentId=studentImpl.getStudentId(userId);
+                    boolean isApproved=studentImpl.isApproved(studentId);
+
+                    if(isApproved){
+                        StudentCRSMenu studentCRSMenu=new StudentCRSMenu();
+                        studentCRSMenu.create_menu(studentId);
+                    }
+                    else{
+                        System.out.println("Failed to login, you have not been approved by the administration!");
+                    }
+                    break;
+            }
+        }
+        else {
+            System.out.println("Invalid credentials");
+        }
+    }
+
+    public void registerStudent() {
+        Scanner sc=new Scanner(System.in);
+        String userId,name,password,address,country,branch;
+        int batch;
+
+        System.out.println("Name:");
+        name=sc.nextLine();
+        System.out.println("Email:");
+        userId=sc.next();
+        System.out.println("Password:");
+        password=sc.next();
+        System.out.println("Batch:");
+        batch=sc.nextInt();
+        System.out.println("Branch:");
+        branch=sc.nextLine();
+        sc.nextLine();
+        System.out.println("Address:");
+        address=sc.nextLine();
+        System.out.println("Country");
+        country=sc.next();
+
+        int studentId=studentImpl.register(name,userId,password,batch,branch,address,country);
+    }
+
+    public void updatePassword() {
+        Scanner sc=new Scanner(System.in);
+        String userId,newPassword;
+        System.out.println("Email");
+        userId=sc.next();
+        System.out.println("New Password:");
+        newPassword=sc.next();
+
+        boolean isUpdated=userImpl.updatePassword(userId, newPassword);
+
+        if(isUpdated)
+            System.out.println("Password updated successfully!");
+
+        else
+            System.out.println("Error while updating. Please try again!");
     }
 }
+
--- CRS-Group7-Flipkart-POS/src/com/flipkart/constant/Role.java
+++ CRS-Group7-Flipkart-POS/src/com/flipkart/constant/Role.java
@@ -4,4 +4,17 @@ public enum Role {
     STUDENT,
     ADMIN,
     PROFESSOR;
+
+    public static Role stringToName(String role)
+    {
+        Role userRole=null;
+
+        if(role.equalsIgnoreCase("ADMIN"))
+            userRole=Role.ADMIN;
+        else if(role.equalsIgnoreCase("PROFESSOR"))
+            userRole=Role.PROFESSOR;
+        else if(role.equalsIgnoreCase("STUDENT"))
+            userRole=Role.STUDENT;
+        return userRole;
+    }
 }
