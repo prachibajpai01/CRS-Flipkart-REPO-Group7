1629c33e5e4ed65aed7b79284cde8bb9d1d03c08
Akashdeep-flipkart <akashdeep.singh@flipkart.com>
2022-06-24 11:24:27 +0530

    added comments

--- CRS-Group7-Flipkart-POS/src/com/flipkart/bean/Grade.java
+++ CRS-Group7-Flipkart-POS/src/com/flipkart/bean/Grade.java
@@ -3,25 +3,40 @@ package com.flipkart.bean;
 // class to store information of grade in a particular course
 
 public class Grade {
-    private int courseId; // every course has an ID
+
+    private String courseId; // every course has an ID
     private String grade; // grade for the course
 
+    // getters and setters for attributes
+
+    /**
+     * @return ID of course
+     */
     public int getCourseId() {
         return courseId;
     }
 
+    /**
+     * Set ID of course
+     * @param courseId
+     */
     public void setCourseId(int courseId) {
         this.courseId = courseId;
     }
 
+    /**
+     * Get grade of course
+     * @return grade of the course
+     */
     public String getGrade() {
         return grade;
     }
 
+    /**
+     * set grade for course
+     * @param grade
+     */
     public void setGrade(String grade) {
         this.grade = grade;
     }
-
-
-
 }
